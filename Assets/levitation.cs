d82b12225bad9dea5c0239807b95b3bae80b180c
Obyka <polier.florian@gmail.com>
2022-03-26 23:17:37 +0100

    random timers on levitation

--- Assets/levitation.cs
+++ Assets/levitation.cs
@@ -8,12 +8,14 @@ public class levitation : MonoBehaviour
     // Position Storage Variables
     Vector3 posOffset = new Vector3 ();
     Vector3 tempPos = new Vector3 ();
-    public bool firstUpdate = true;
+    float timeEllapsed;
+    bool levitate = false;
     public float amplitude = 0.5f; 
     // Use this for initialization
     void Start () {
         // Store the starting position & rotation of the object
         posOffset = transform.position;
+        StartCoroutine(setLevitation());
     }
      
     // Update is called once per frame
@@ -21,18 +23,20 @@ public class levitation : MonoBehaviour
         // Spin object around Y-Axis
         // transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
  
-        if(firstUpdate){
-            StartCoroutine(waiter());
-            firstUpdate = false;
+        if(levitate == true){
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin (timeEllapsed * Mathf.PI * frequency) * amplitude;
+            
+            transform.position = tempPos;
+            timeEllapsed += Time.deltaTime;
         }
         // Float up/down with a Sin()
-        tempPos = posOffset;
-        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
- 
-        transform.position = tempPos;
+
     }
-    IEnumerator waiter(){
-        float wait_time = Random.Range (1f, 3f);
+    IEnumerator setLevitation(){
+        float wait_time = Random.Range (1f, 2f);
         yield return new WaitForSeconds (wait_time);
+        levitate = true;
+        timeEllapsed = 0;
     }
 }
