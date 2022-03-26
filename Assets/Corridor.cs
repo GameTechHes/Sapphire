01455cb791dc255f0b3c2dd0694641cd3aa8a248
Gil Balsiger <gil.balsiger@gmail.com>
2022-03-26 18:35:56 +0100

    Add demo generation with corridor

---
+++ Assets/Corridor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corridor : MonoBehaviour
+{
+    public float length;
+
+    public Transform start;
+
+    public Transform end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
