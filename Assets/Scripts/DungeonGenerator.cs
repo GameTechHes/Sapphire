f81cf47c8020efda33d0490d9caa324779da4265
Gil Balsiger <gil.balsiger@gmail.com>
2022-03-30 15:27:15 +0200

    feat(procedural): add base square room

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,49 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    public List<Room> rooms;
+    public List<Room> roomPrefabs;
 
-    public Corridor corridor;
+    public Corridor corridorPrefab;
 
-    public int roomCount;
+    public int totalRoomCount;
+
+    private int roomCount = 0;
+    private const float SCALE_FACTOR = 18.0f * 2.0f;
+    
+    private Stack<RoomToInstantiate> roomsToInstantiate = new Stack<RoomToInstantiate>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Corridor lastCorridor = null;
+        InstantiateRoom(roomPrefabs[0], 0, 0);
+        // grid = new Room[gridSize.x, gridSize.y];
+        //
+        //
+        // for (int i = 0; i < gridSize.x; i++)
+        // {
+        //     for (int j = 0; j < gridSize.y; j++)
+        //     {
+        //         if (roomCount == totalRoomCount)
+        //         {
+        //             break;
+        //         }
+        //
+        //         roomCount++;
+        //         var room = Instantiate(roomPrefabs[0], new Vector3(i * SCALE_FACTOR, 0.0f, j * SCALE_FACTOR),
+        //             Quaternion.identity).GetComponent<Room>();
+        //         foreach (var door in room.GetActiveDoors())
+        //         {
+        //             var corridor = Instantiate(corridorPrefab, door).GetComponent<Corridor>();
+        //             corridor.transform.position += corridor.transform.position - corridor.start.position;
+        //         }
+        //     }
+        }
 
-
-        for (int i = 0; i < roomCount; i++)
+        void InstantiateRoom(Room roomPrefab, int x, int z)
         {
-            
-            Room generatedRoom =
-                Instantiate(rooms[i].gameObject).GetComponent<Room>();
-            if (lastCorridor != null)
+            roomCount++;
+            var room = Instantiate(roomPrefabs[0], new Vector3(x * SCALE_FACTOR, 0.0f, z * SCALE_FACTOR),
+                Quaternion.identity).GetComponent<Room>();
+            foreach (var door in room.GetActiveDoors())
             {
-                generatedRoom.transform.rotation = lastCorridor.end.rotation * generatedRoom.doors[0].rotation;
-                generatedRoom.transform.position = lastCorridor.end.transform.position -
-                                                   generatedRoom.doors[0].position;
-            }
-
-            Transform roomDoor;
-            if (generatedRoom.doors.Count > 1)
-            {
-                roomDoor = generatedRoom.doors[1];
-            }
-            else
-            {
-                roomDoor = generatedRoom.doors[0];
-            }
-            roomDoor.Translate(Vector3.forward * (corridor.length / 2), Space.Self);
-            if (i < roomCount - 1)
-            {
-                lastCorridor = Instantiate(corridor, roomDoor);
+                var corridor = Instantiate(corridorPrefab, door).GetComponent<Corridor>();
+                corridor.transform.position += corridor.transform.position - corridor.start.position;
+                var xFactor = Mathf.RoundToInt(Vector3.Dot(corridor.end.transform.forward, Vector3.forward));
+                var zFactor = Mathf.RoundToInt(Vector3.Dot(corridor.end.transform.forward, Vector3.right));
+                if (roomCount < totalRoomCount)
+                {
+                    InstantiateRoom(roomPrefabs[0], x + xFactor, z + zFactor);
+                }
             }
         }
-    }
+
+        // Corridor lastCorridor = null;
+        //
+        //
+        // for (int i = 0; i < roomCount; i++)
+        // {
+        //     
+        //     Room generatedRoom =
+        //         Instantiate(rooms[i].gameObject).GetComponent<Room>();
+        //     if (lastCorridor != null)
+        //     {
+        //         generatedRoom.transform.rotation = lastCorridor.end.rotation * generatedRoom.doors[0].rotation;
+        //         generatedRoom.transform.position = lastCorridor.end.transform.position -
+        //                                            generatedRoom.doors[0].position;
+        //     }
+        //
+        //     Transform roomDoor;
+        //     if (generatedRoom.doors.Count > 1)
+        //     {
+        //         roomDoor = generatedRoom.doors[1];
+        //     }
+        //     else
+        //     {
+        //         roomDoor = generatedRoom.doors[0];
+        //     }
+        //     roomDoor.Translate(Vector3.forward * (corridor.length / 2), Space.Self);
+        //     if (i < roomCount - 1)
+        //     {
+        //         lastCorridor = Instantiate(corridor, roomDoor);
+        //     }
+        // }
+    // }
 
 
 // Update is called once per frame
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,15 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public List<Transform> doors;
+    public GameObject doorModel;
+
+    [Header("Doors transforms")]
+    public Transform door1;
+    public Transform door2;
+    public Transform door3;
+    public Transform door4;
+
+    [Header("Doors activation")]
+    public bool isDoor1Active;
+    public bool isDoor2Active;
+    public bool isDoor3Active;
+    public bool isDoor4Active;
+    
+    [Header("Doors initial state")]
+    public bool isDoor1Present;
+    public bool isDoor2Present;
+    public bool isDoor3Present;
+    public bool isDoor4Present;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!isDoor1Active || isDoor1Present)
+        {
+            var door = Instantiate(doorModel, door1);
+            door.transform.Translate(Vector3.left * 0.675f + Vector3.back * 0.1f, Space.Self);
+        }
+        if (!isDoor2Active || isDoor2Present)
+        {
+            var door = Instantiate(doorModel, door2);
+            door.transform.Translate(Vector3.left * 0.675f + Vector3.back * 0.1f, Space.Self);
+        }
+        if (!isDoor3Active || isDoor3Present)
+        {
+            var door = Instantiate(doorModel, door3);
+            door.transform.Translate(Vector3.left * 0.675f + Vector3.back * 0.1f, Space.Self);
+        }
+        if (!isDoor4Active || isDoor4Present)
+        {
+            var door = Instantiate(doorModel, door4);
+            door.transform.Translate(Vector3.left * 0.675f + Vector3.back * 0.1f, Space.Self);
+        }
+    }
+
+    public List<Transform> GetActiveDoors()
+    {
+        var activeDoors = new List<Transform>();
+        if(isDoor1Active) activeDoors.Add(door1);
+        if(isDoor2Active) activeDoors.Add(door2);
+        if(isDoor3Active) activeDoors.Add(door3);
+        if(isDoor4Active) activeDoors.Add(door4);
+        return activeDoors;
     }
 
     // Update is called once per frame
