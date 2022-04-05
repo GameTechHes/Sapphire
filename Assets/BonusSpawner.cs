62b96adeb04cda1d746cc391d81c700bf2c42f4c
Obyka <polier.florian@gmail.com>
2022-04-05 21:31:40 +0200

    Arrow model added

--- Assets/BonusSpawner.cs
+++ Assets/BonusSpawner.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class BonusSpawner : MonoBehaviour
 {
     public List<PowerUpBase> powerupPrefabs;
+    public int threshold;
     // Start is called before the first frame update
     void Start()
     {
         foreach(GameObject spawnPoint in GameObject.FindGameObjectsWithTag("SpawningPoint")){
-            Debug.Log(spawnPoint.transform.position.x);
-            Vector3 spawnPosition = spawnPoint.transform.position;
-            spawnPosition.y += 2;
-            Instantiate(powerupPrefabs[0], spawnPosition, Quaternion.identity);
+            float randomNumber = Random.Range(0, 100);
+            if(randomNumber >= threshold){
+                Debug.Log(spawnPoint.transform.position.x);
+                Vector3 spawnPosition = spawnPoint.transform.position;
+                spawnPosition.y += 1;
+                Instantiate(powerupPrefabs[0], spawnPosition, Quaternion.identity);
+            }
         }
     }
 
