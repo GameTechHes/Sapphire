d27112713a9e53d9c375fc81cee0a15b66febd15
Raemy Mathis <mathis.raemy@gmail.com>
2022-04-07 16:35:43 +0200

    Ajout de la detection du joueur

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -15,11 +15,7 @@ public class Bot : MonoBehaviour
     [SerializeField] private float angleMax;
 
     private Animator animator;
-
-    private float floorMinX;
-    private float floorMaxX;
-    private float floorMinZ;
-    private float floorMaxZ;
+    private bool hasSeenPlayer = false;
 
     private NavMeshAgent agent;
     private float wanderTimer;
@@ -34,7 +30,12 @@ public class Bot : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(lookPos);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 2);
     }
-
+    public void setHasSeenPlayer(bool val){
+        hasSeenPlayer = val;
+    }
+    public bool getHasSeenPlayer(){
+        return hasSeenPlayer;
+    }
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -46,29 +47,35 @@ public class Bot : MonoBehaviour
         // floorMinZ = floor.GetComponent<MeshFilter>().mesh.bounds.min.z * floor.transform.localScale.z;
         // floorMaxZ = floor.GetComponent<MeshFilter>().mesh.bounds.max.z * floor.transform.localScale.z;
     }
+    public void setNewDestination(Vector3 dest){
+        NavMeshHit navHit;
+        if (NavMesh.SamplePosition(dest, out navHit, wanderRadius, NavMesh.AllAreas))
+        {
+            Vector3 newPos = navHit.position;
+            animator.SetBool("walking", true);
+            agent.SetDestination(newPos);
+            timer = 0;
+            wanderTimer = Random.Range(minWanderTimer, maxWanderTimer);
+            print("New target");
+        }
+    }
+    public void resetCurrentPath(){
+        agent.ResetPath();
+    }
 
     void Update()
     {
         if (!GetComponent<NavMeshAgent>().enabled) return;
-        if (agent.remainingDistance - agent.stoppingDistance < 0.1)
+        if (agent.remainingDistance - agent.stoppingDistance < 0.1 )
         {
             if (animator.GetBool("walking")) animator.SetBool("walking", false);
-
-            timer += Time.deltaTime;
-
-            if (timer >= wanderTimer)
-            {
-                Vector3 randDirection = Random.insideUnitSphere * wanderRadius;
-                randDirection += transform.position;
-                NavMeshHit navHit;
-                if (NavMesh.SamplePosition(randDirection, out navHit, wanderRadius, NavMesh.AllAreas))
+            if(!hasSeenPlayer){
+                timer += Time.deltaTime;
+                if (timer >= wanderTimer)
                 {
-                    Vector3 newPos = navHit.position;
-                    animator.SetBool("walking", true);
-                    agent.SetDestination(newPos);
-                    timer = 0;
-                    wanderTimer = Random.Range(minWanderTimer, maxWanderTimer);
-                    print("New target");
+                    Vector3 randDirection = Random.insideUnitSphere * wanderRadius;
+                    randDirection += transform.position;
+                    this.setNewDestination(randDirection);
                 }
             }
         }
--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -6,9 +6,11 @@ public class FieldOfView : MonoBehaviour
 {
     public int viewAngle;
     public float detectionRadius;
-    // Start is called before the first frame update
+    private float time = 0;
+    private Bot bot;
     void Start()
     {
+        bot = GetComponent<Bot>();
         if(viewAngle > 90) viewAngle = 90;
         
     }
@@ -28,9 +30,30 @@ public class FieldOfView : MonoBehaviour
                 print("Detected");
                 Vector3 direction = hitCollider.gameObject.transform.position - transform.position;
                 Vector2 direction2D = new Vector2(direction.x,direction.z);
+                if(direction2D.magnitude < 1) {
+                    print("TOUCH");
+                    bot.resetCurrentPath();
+                    return;
+                }
                 Vector2 fw2D = new Vector2(transform.forward.x, transform.forward.z);
                 float angle = Vector2.Angle(fw2D, direction2D);
                 if (angle < viewAngle){
+                    if(!bot.getHasSeenPlayer() || time > 2){
+                        bot.resetCurrentPath();
+                        bot.setNewDestination( hitCollider.gameObject.transform.position);
+                        bot.setHasSeenPlayer(true);
+                        time = 0;
+                        print("setting new target");
+                    }else{
+                        time += Time.deltaTime;
+                        if(time > 10){
+                            bot.setHasSeenPlayer(false);
+                            print("Oh i lost player");
+                        }
+                    }
+                    //TODO call hitCollider.gameObject.setDest()
+                    //Refresh path every x time
+                    //reset current path
                     print("SEEN !!!");
                 }
             }
