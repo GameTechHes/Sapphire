0185daa64ccdf9bcfdfc8115bbd2b1a94f62a37b
Obyka <polier.florian@gmail.com>
2022-04-07 13:00:19 +0200

    Prepare to conflict

---
+++ Assets/scaleMinicam.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleMinicam : MonoBehaviour
+{
+    float distanceX = 0;
+    float distanceZ = 0;
+    Camera minicam;
+    public GameObject camUI;
+    RectTransform rectTransform;
+    Quaternion originalRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minicam = GetComponent<Camera>();
+        originalRotation = minicam.transform.rotation;
+
+        if(camUI){
+            Debug.Log("CamUI found");
+        }
+        rectTransform = camUI.GetComponent<RectTransform>();
+        float minX = 10000f;
+        float minZ = 10000f;
+        float maxX = -10000f;
+        float maxZ = -10000f;
+        foreach(GameObject refPoint in GameObject.FindGameObjectsWithTag("ReferencePoint")){
+             minX = refPoint.transform.position.x < minX ?  refPoint.transform.position.x : minX;
+             minZ = refPoint.transform.position.z < minZ ?  refPoint.transform.position.z : minZ;
+             maxX = refPoint.transform.position.x > maxX ?  refPoint.transform.position.x : maxX;
+             maxZ = refPoint.transform.position.z > maxZ ?  refPoint.transform.position.z : maxZ;
+        }
+        Debug.Log("Distance X" + (maxX - minX).ToString());
+        Debug.Log("Distance Z" + (maxZ - minZ).ToString());
+        distanceZ = maxZ - minZ;
+        distanceX = maxX - minX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minicam.transform.rotation = originalRotation;
+        if(distanceX < distanceZ){
+            minicam.orthographicSize = 0.5f * distanceZ;
+        } else{
+            float unitsPerPixel = distanceX / rectTransform.rect.width;
+            float desiredHalfHeight = 0.5f * unitsPerPixel * rectTransform.rect.height;
+            minicam.orthographicSize = desiredHalfHeight;
+        }
+    }
+}
