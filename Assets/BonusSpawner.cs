99a6eb08929937f3c248e7fd2d0c112329d7657a
Gil Balsiger <gil.balsiger@gmail.com>
2022-04-07 10:50:27 +0200

    refactor(shoot): refactor the shooting system

--- Assets/BonusSpawner.cs
+++ Assets/BonusSpawner.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,17 +11,10 @@ public class BonusSpawner : MonoBehaviour
         foreach(GameObject spawnPoint in GameObject.FindGameObjectsWithTag("SpawningPoint")){
             float randomNumber = Random.Range(0, 100);
             if(randomNumber >= threshold){
-                Debug.Log(spawnPoint.transform.position.x);
                 Vector3 spawnPosition = spawnPoint.transform.position;
                 spawnPosition.y += 1;
                 Instantiate(powerupPrefabs[0], spawnPosition, Quaternion.identity);
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
