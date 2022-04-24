0d4b028f8dd074d24fa58122b45238747dea894a
Gil Balsiger <gil.balsiger@gmail.com>
2022-04-24 17:37:50 +0200

    feat(aim): improve shooting direction, add crosshair to UI

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,3 +1,6 @@
+using System;
+using Cinemachine;
+using StarterAssets;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +10,12 @@ public class Knight : MonoBehaviour
     public int maxHealth = 100;
 
     public HealthBar healthBar;
+    public CinemachineVirtualCamera followCamera;
+    public StarterAssetsInputs inputs;
     private Animator _controller;
+
+    public float AimSpeed = 4.0f;
+    private float _cameraDistance = 4.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,20 @@ public class Knight : MonoBehaviour
         healthBar.SetMaxHealth(maxHealth);
         healthBar.SetProgress(_health);
         _controller = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (inputs.aim)
+        {
+            _cameraDistance = Mathf.Lerp(_cameraDistance, 2.0f, Time.deltaTime * AimSpeed);
+            followCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>().CameraDistance = _cameraDistance;
+        }
+        else
+        {
+            _cameraDistance = Mathf.Lerp(_cameraDistance, 4.0f, Time.deltaTime * AimSpeed);
+            followCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>().CameraDistance = _cameraDistance;
+        }
     }
 
     void OnAim(InputValue value)
